9a4bffa8fd7c08e8b2fb065e348b07c71d3a7dec
QuanTT <thaithienquan09@gmail.com>
2025-05-14 23:49:55 +0700

    fragment learning 2

--- src/demo/FragmentTotur.tsx
+++ src/demo/FragmentTotur.tsx
@@ -2,11 +2,14 @@ import { Fragment } from "react"
 
 export default function FragmentTotur() {
     return (
+      <>
+      <div> this not in the fragment tag</div>
       <Fragment>
       <div>
         <h1>Title</h1>
          <p>This is a paragraph within a fragment.</p>
       </div>
       </Fragment>
+</>
     )
 }
