469c6525f5814fa231d25f4a519d01cc9b716b3c
QuanTT <thaithienquan09@gmail.com>
2025-05-20 23:58:21 +0700

    demo react router 2

--- src/demo/DemoReactRouter.tsx
+++ src/demo/DemoReactRouter.tsx
@@ -1,13 +1,14 @@
-import {BrowserRouter as Route, Router, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "../RouterConnect/Home.tsx";
 
-export default  function DemoReactRouter(){
-    return(<>
-        <Router location={""} navigator={undefined}  >
+export default function DemoReactRouter() {
+    return (<>
+        <Router>
             <Routes>
-                <Route path={"/about"} element={<About/>} />
-                <Route path={"/home"} element={<Home/>} />
-            </Routes>s
+                <Route path={"/about"} element={<About/>}/>
+                <Route path={"/home"} element={<Home/>}/>
+                <Route path={"/*"} element={<NotFound/>}/>
+            </Routes>
         </Router>
     </>)
 }
