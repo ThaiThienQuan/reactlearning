ef57db90fb0eddf0f677c4409a8daac0522e01a4
QuanTT <thaithienquan09@gmail.com>
2025-05-21 23:59:38 +0700

    demo react usecontext next

--- src/demo/DemoReactRouter.tsx
+++ src/demo/DemoReactRouter.tsx
@@ -1,13 +1,13 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "../RouterConnect/Home.tsx";
+import About from "../RouterConnect/About.tsx";
 
 export default function DemoReactRouter() {
     return (<>
         <Router>
             <Routes>
                 <Route path={"/about"} element={<About/>}/>
-                <Route path={"/home"} element={<Home/>}/>
-                <Route path={"/*"} element={<NotFound/>}/>
+                <Route path={"/home"||"/tu"} element={<Home/>}/>
             </Routes>
         </Router>
     </>)
