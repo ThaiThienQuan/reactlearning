5dc724b970be62304b67a62c1f78859150ccc19b
QuanTT <thaithienquan09@gmail.com>
2025-05-30 23:40:06 +0700

    demo Theme Context

--- src/demo/DemoUseEffect.tsx
+++ src/demo/DemoUseEffect.tsx
@@ -1,30 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 
 export default function DataFetching() {
     const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
-
+    const [loading, setLoading] = useState(true)
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then((response) => response.json())
             .then((data) => {
                 setData(data);
                 setLoading(false);
-            });
+            })
     }, []);
-
     return (
         <div className="container mt-5">
-            <h2>Data Fetching</h2>
-            {loading ? (
-                <p>Loading...</p>
-            ) : (
-                <ul>
-                    {data.slice(0, 5).map((item) => (
+            <h3>Data fetching</h3>
+            {loading ? <p>Loading ...</p>
+                : (
+                    <ul>{data.slice(0.5).map((item) => (
                         <li key={item.id}>{item.title}</li>
                     ))}
-                </ul>
-            )}
-        </div>
-    );
+                    </ul>)}
+        </div>);
 }
