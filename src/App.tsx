8a5d985b763a06b84e8b06faffc37e9619c60ca4
QuanTT <thaithienquan09@gmail.com>
2025-06-07 21:57:10 +0700

    demo Employee Management with useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,4 @@
-import {useState} from "react";
-import UserProfile from "./demo/UserProfile.tsx";
-import HandlingEvent from "./demo/Handling-event.tsx";
-import ConditionRD from "./demo/ConditionRD.tsx";
-import Listrendering from "./demo/Listrendering.tsx";
-import PlayerList from "./demo/PlayerList.tsx";
-import TodoList from "./demo/TodoList.tsx";
-import styles from "./demo/ModuleCSS.module.css";
-import Lifecycle from "./demo/Lifecycle.tsx";
-import Form from "./demo/Form.tsx";
-import InputValidate from "./demo/InputValidate.tsx";
 import CompletedValidate from "./demo/CompletedValidate.tsx";
-import FragmentTotur from "./demo/FragmentTotur.tsx";
-import {MyProvider} from "./assets/MyContext.tsx";
-import DemoUseContext from "./demo/demoUseContext.tsx";
-import DemoReactRouter from "./demo/DemoReactRouter.tsx";
-import DemoUsecontext_NestCom from "./demo/DemoUsecontext_NestCom.tsx";
 import DemoUseEffect from "./demo/DemoUseEffect.tsx";
 import ThemeComponent from "./assets/ThemeComponent.tsx";
 import {ThemeProvider} from "./assets/ThemeContext.tsx";
@@ -24,21 +8,7 @@ function App() {
 
     return (
         <>
-            {/*<DemoReactRouter/>*/}
-            {/*<FragmentTotur/>*/}
-            {/* <UserProfile email={"thaithienquan@gmail.com"} name={"Quan"} gender={true}></UserProfile>
-        <HandlingEvent/>
-        <ConditionRD/>
-        <Listrendering/>
-        <PlayerList/>
-        <TodoList/>
-        <Lifecycle/> */}
-            {/*<Form/>*/}
-            {/*<InputValidate/>*/}
             <CompletedValidate/>
-            {/*<MyProvider>*/}
-            {/*  <DemoUseContext/>*/}
-            {/*  </MyProvider>*/}
             <DemoUseEffect/>
             <ThemeProvider>
                 <ThemeComponent/>
--- src/demo/DemoUseState.tsx
+++ src/demo/DemoUseState.tsx
@@ -1,18 +1,48 @@
-import { useState } from "react"
+import {useState} from "react";
 
-export default function demoUseState(){
-    const [count, setCount] = useState(0)
-    const Handleclick=()=>{
-        setCount(count+1);
+export default function DemoUseState() {
+    const [employees, setEmployees] = useState([])
+    const [inputvalue, setInputvalue] = useState({
+        id: '',
+        firstName: '',
+        lastName: '',
+        email: ''
+    })
+    const AddEmployee = (e) => {
+        e.preventDefault();
     }
-    return(<>
-        <div className="container mt-5">
-            <h2>Counter</h2>
-            <p>Current Count: {count}</p>
-            <button className="btn btn-primary"
-                    onClick={Handleclick}>
-                Increment
+    return (<>
+        <form onSubmit={AddEmployee}>
+            <input type="text" className={`form-control`} value={inputvalue.id}
+                   onChange={(e) => setInputvalue({
+                       ...inputvalue,
+                       id: e.target.value
+                   })} placeholder={`ID`}/>
+            <input type="text" className={`form-control`}
+                   value={inputvalue.firstName}
+                   onChange={(e) => setInputvalue({
+                       ...inputvalue,
+                       firstName: e.target.value
+                   })} placeholder={`First Name`}/>
+            <input type="text" className={`form-control`}
+                   value={inputvalue.lastName}
+                   onChange={(e) => setInputvalue({
+                       ...inputvalue,
+                       lastName: e.target.value
+                   })} placeholder={`Last name`}/>
+            <input type="text" className={`form-control`}
+                   value={inputvalue.email}
+                   onChange={(e) => setInputvalue({
+                       ...inputvalue,
+                       email: e.target.value
+                   })} placeholder={`Email`}/>
+            <button type="submit" className={`btn btn-primary`}>Add Employee
             </button>
-        </div>
+        </form>
+        <table className=" mt-3">
+            <tr className="list-group mt-3">
+                {employees.map(employees, index)=>()}
+            </tr>
+        </table>
     </>)
 }
