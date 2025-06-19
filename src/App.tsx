71ea17256ee7cb077d52df10cd4b57de4ef17690
QuanTT <thaithienquan09@gmail.com>
2025-06-19 21:39:57 +0700

    Demo  useRef 1.01

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import LocalStorageComponent from "./assets/LocalStorageComponent.tsx";
 import DemoUseState from "./demo/DemoUseState.tsx";
 import Timer from "./demo/Timer.tsx";
 import {ThemeProvider} from "./assets/ThemeContext.tsx";
+import DemoUseRef from "./demo/DemoUseRef.tsx";
 
 function App() {
 
     return (
         <>
             {/*<CompletedValidate/>*/}
+            <DemoUseRef/>
             <Timer/>
             <DemoUseState/>
             <LocalStorageComponent/>
--- src/demo/DemoUseRef.tsx
+++ src/demo/DemoUseRef.tsx
@@ -1,14 +1,55 @@
-import {useRef} from "react";
+import { useRef } from "react";
 
 export default function DemoUseRef() {
-    const firstName=useRef(null);
-    return (<>
-        <form action="" method="get">
-            <div>
-                <label htmlFor={'firstName'}></label>
-                <input ref={firstName} id={'firstName'} type="text" className={`form-control`}/>
-            </div>
-        </form>
-    </>
-)
+  const firstName = useRef(null);
+  const lastName = useRef(null);
+  const email = useRef(null);
+  const Submit = (e) => {
+    e.preventDefault();
+   alert(`First Name: ${firstName.current.value}\nLast Name: ${lastName.current.value}\nEmail: ${email.current.value}`);
+ };
+  const focusFirstname = () => {
+    firstName.current.focus();
+  };
+  return (
+    <div className={`container`}>
+      <form onClick={Submit}>
+        <div className={`form-group`}>
+          <label htmlFor={"firstName"}></label>
+          <input
+            ref={firstName}
+            id={"firstName"}
+            type="text"
+            className={`form-control`}
+          />
+        </div>
+        <div className={`form-group`}>
+          <label htmlFor={"lastName"}></label>
+          <input
+            ref={lastName}
+            id={"lastName"}
+            type="text"
+            className={`form-control`}
+          />
+        </div>
+        <div className={`form-group`}>
+          <label htmlFor={"email"}></label>
+          <input
+            ref={email}
+            id={"email"}
+            type="text"
+            className={`form-control`}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+        <button
+          type="button"
+          className="btn btn-secondary"
+          onClick={focusFirstname}
+        >
+          Focus First Name
+        </button>
+      </form>
+    </div>
+  );
 }
