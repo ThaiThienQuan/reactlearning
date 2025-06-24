f92ded24a4d6de0828beb9def97eed9e1ffba623
QuanTT <thaithienquan09@gmail.com>
2025-06-24 23:43:00 +0700

    Demo useId with useRef 1.01

--- src/demo/DemoUseId_useRef.tsx
+++ src/demo/DemoUseId_useRef.tsx
@@ -1,17 +1,35 @@
-import { useId, useRef, useState , useEffect } from "react";
+import { useId, useRef, useState, useEffect } from "react";
 
 export default function DemoUseId_useRef() {
   const firstName = useRef(null);
-  const id = useId();
+  const lastName = useRef(null);
+  const email = useRef(null);
+  const firstNameId = useId();
+  const emailId = useId();
+  const lastNameId = useId();
   const firstNameId = `${id}-firstName`;
   const [data, setdata] = useState({
     firstName: "",
+    lastName: "",
+    email: "",
   });
   useEffect(() => {
     firstName.current.focus();
     console.log(firstName.current);
-    
   }, []);
+  const handlechange = (e) => {
+    const { name, value } = e.target;
+    setdata((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(
+      `Thông tin đã gửi:\nTên đệm: ${data.firstName}\n Tên :${data.lastName}\nEmail: ${data.email}`
+    );
+  };
 
   return (
     <>
