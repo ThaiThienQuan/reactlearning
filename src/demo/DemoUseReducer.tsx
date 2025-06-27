22a89a7e35406a86aaef8f3851c9e360d15b0fd5
QuanTT <thaithienquan09@gmail.com>
2025-06-27 23:34:13 +0700

    Demo useReducer 1.01

--- src/demo/DemoUseReducer.tsx
+++ src/demo/DemoUseReducer.tsx
@@ -1,9 +1,11 @@
+import { useReducer } from "react";
+
 const initialState = { count: 0 };
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'increment':
+    case "increment":
       return { count: state.count + 1 };
-    case 'decrement':
+    case "decrement":
       return { count: state.count - 1 };
 
     default:
@@ -11,11 +13,13 @@ const reducer = (state, action) => {
   }
 };
 export default function DemoUseReducer() {
-  const [state, dispatch] = useReducer(initialState, reducer);
+  const [state, dispatch] = useReducer(reducer,initialState);
 
-  return( <>
-  <p>Count: {state.count}</p>
-  <button onClick={()=>dispatch({type:'increment'})}>Increase</button>
-  <button onClick={()=>dispatch({type:'decrement'})}>Decrease</button>
-  </>);
+  return (
+    <>
+      <p>Count: {state.count}</p>
+      <button onClick={() => dispatch({ type: "increment" })}>Increase</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>Decrease</button>
+    </>
+  );
 }
