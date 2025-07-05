4fd7d9937281406ff500902d954d4529bb66af3b
QuanTT <thaithienquan09@gmail.com>
2025-07-05 23:23:38 +0700

    Demo useCallback 1.01

--- src/demo/DemoUseCallback.tsx
+++ src/demo/DemoUseCallback.tsx
@@ -1,12 +1,17 @@
+import { useCallback, useState } from "react";
 
-export default function DemoUseCallback(){
-    useCallback(
-      () => {
-        first
-      },
-      [second],
-    )
-    
-    return(<>
-        </>)
-} 
+export default function DemoUseCallback() {
+  const [count, setcount] = useState(0);
+  const increment = useCallback(() => {
+    setcount((prev) => prev + 1);
+  }, []);
+
+  return (
+    <>
+      <div>
+        <p>Count: {count}</p>
+        <button onClick={increment}>Increment</button>
+      </div>
+    </>
+  );
+}
