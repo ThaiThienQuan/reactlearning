a67ce760eedc9105a7cefc920acfbb6e20cd724f
QuanTT <thaithienquan09@gmail.com>
2025-09-03 22:26:46 +0700

    test useContext

--- src/assets/ThemeComponent.tsx
+++ src/assets/ThemeComponent.tsx
@@ -2,7 +2,7 @@ import {useContext} from "react";
 import ThemeContext from "./ThemeContext.tsx";
 
 export default function ThemeComponent(){
-    const {theme,ChangeTheme} = useContext(ThemeContext)
+     const {theme,ChangeTheme} = useContext(ThemeContext)
      return(<>
      <div className={`container mt-5 ${theme == 'dark' ? 'bg-dark text-white' : 'bg-light text-dark'}`}>
          <button onClick={ChangeTheme}>Change Theme</button>
